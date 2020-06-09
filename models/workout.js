2dc85e18d8d309bd79215d914857e7f97c31de86
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-06-08 22:55:25 -0500

    server set up and started mongoose model

---
+++ models/workout.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    excercises: {
+        type: {
+            type: String,
+            trim: true,
+        },
+        name: {
+            type: String,
+            required: "Please enter name of workout.",
+            minlength: 1
+        },
+        duration: {
+            type: Number,
+            required: "Enter duration.",
+            min: 1,
+        },
+        weight: {
+            type: Number,
+            required: "Enter amount of weight.",
+            min: 1,
+        },
+        reps: {
+            type: Number,
+            required: "Enter amount of reps.",
+            min: 1,
+        },
+        sets: {
+            type: Number,
+            required: "Enter amount of sets.",
+            min: 1,
+        }
+    }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
