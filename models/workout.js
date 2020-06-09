3a5b85c8255da2be7b9f66d1b7d315a9537fef75
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-06-08 23:29:34 -0500

    successful connection to mongodb

--- models/workout.js
+++ models/workout.js
@@ -8,37 +8,39 @@ const workoutSchema = new Schema({
         type: Date,
         default: Date.now
     },
-    excercises: {
-        type: {
-            type: String,
-            trim: true,
-        },
-        name: {
-            type: String,
-            required: "Please enter name of workout.",
-            minlength: 1
-        },
-        duration: {
-            type: Number,
-            required: "Enter duration.",
-            min: 1,
-        },
-        weight: {
-            type: Number,
-            required: "Enter amount of weight.",
-            min: 1,
-        },
-        reps: {
-            type: Number,
-            required: "Enter amount of reps.",
-            min: 1,
-        },
-        sets: {
-            type: Number,
-            required: "Enter amount of sets.",
-            min: 1,
+    excercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                required: "Please enter name of workout.",
+                minlength: 1
+            },
+            duration: {
+                type: Number,
+                required: "Enter duration.",
+                min: 1,
+            },
+            weight: {
+                type: Number,
+                required: "Enter amount of weight.",
+                min: 1,
+            },
+            reps: {
+                type: Number,
+                required: "Enter amount of reps.",
+                min: 1,
+            },
+            sets: {
+                type: Number,
+                required: "Enter amount of sets.",
+                min: 1,
+            }
         }
-    }
+    ]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
