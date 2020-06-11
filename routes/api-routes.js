cc58f2800e599a8ef1d7cec1407db1cb91e2397d
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-06-10 22:03:39 -0500

    successful api routes for getting stats of weekly workouts and last workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,30 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const db = require("../models/workout.js");
 
-// router.get("/", (req, res) => {
-//     console.log("success");
-// })
+// ROUTE FOR GETTING STATS DATA FOR 7 DAYS
+router.get("/api/workouts/range", (req, res) => {
+    db.find({})
+    .limit(7)
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// ROUTE FOR GETTING LAST WORKOUT
+router.get("/api/workouts", (req,res) => {
+    db.find({})
+    .then(data => {        
+        res.json(data);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+});
+
+
+
 
 module.exports = router;
